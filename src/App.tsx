565ea0bbaeaa6b1f3a79a5525250ad9ca3d282a7
Nata <nata.m.nata@inbox.ru>
2023-10-08 10:34:12 +0300

    add settings

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './styles.css'
 export const App = () => {
   return (
     <>
-      <h1>Сайт в разработке</h1>
+      <h1>Сайт в разработке!!!</h1>
     </>
   )
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,6 +3,11 @@ const webpack = require('webpack')
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: true,
+    port: 8080,
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.name': JSON.stringify('Vishwas'),
