e07d0264c9ea7faca42d2695462979211e924ba2
Nata <nata.m.nata@inbox.ru>
2023-10-11 14:19:51 +0300

    add ModuleFederation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import React from 'react'
 import './styles.css'
+
+const Users = React.lazy(() => import('MicroFrontendApp/UsersComponent'))
 
 export const App = () => {
   return (
     <>
       <h1>Сайт в разработке!!!</h1>
+      <Users/>
     </>
   )
 }
